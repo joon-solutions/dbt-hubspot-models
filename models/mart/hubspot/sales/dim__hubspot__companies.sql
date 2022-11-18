8de4da5afbcff49a14301af8e68cf01966dd186e
Na <nanguyenbg@gmail.com>
2022-11-18 16:06:11 +0700

    update sqlfluffignore

--- models/mart/hubspot/sales/dim__hubspot__companies.sql
+++ models/mart/hubspot/sales/dim__hubspot__companies.sql
@@ -9,50 +9,50 @@ with companies as (
 
 {% if fivetran_utils.enabled_vars(vars=["hubspot__engagement", "hubspot__engagement_company"]) %}
 
-engagements as (
+    engagements as (
 
-    select *
-    from {{ ref('base__hubspot__engagement') }}
+        select *
+        from {{ ref('base__hubspot__engagement') }}
 
-),
+    ),
 
-engagement_companies as (
+    engagement_companies as (
 
-    select *
-    from {{ ref ('base__hubspot__engagement_company') }}
+        select *
+        from {{ ref ('base__hubspot__engagement_company') }}
 
-),
+    ),
 
-engagement_companies_joined as (
+    engagement_companies_joined as (
 
-    select
-        engagements.engagement_type,
-        engagement_companies.company_id
-    from engagements
-    inner join engagement_companies on engagements.engagement_id = engagement_companies.engagement_id
+        select
+            engagements.engagement_type,
+            engagement_companies.company_id
+        from engagements
+        inner join engagement_companies on engagements.engagement_id = engagement_companies.engagement_id
 
-),
+    ),
 
-engagement_companies_agg as (
+    engagement_companies_agg as (
 
     {{ hubspot_engagements_agg('engagement_companies_joined', 'company_id') }}
 
-),
+    ),
 
-joined as (
+    joined as (
 
-    select
-        companies.*,
-        {% for metric in engagement_metrics() %}
-            coalesce(engagement_companies_agg.{{ metric }}, 0) as {{ metric }} {% if not loop.last %},{% endif %}
-        {% endfor %}
-    from companies
-    left join engagement_companies_agg on companies.company_id = engagement_companies_agg.company_id
+        select
+            companies.*,
+            {% for metric in engagement_metrics() %}
+                coalesce(engagement_companies_agg.{{ metric }}, 0) as {{ metric }} {% if not loop.last %},{% endif %}
+            {% endfor %}
+        from companies
+        left join engagement_companies_agg on companies.company_id = engagement_companies_agg.company_id
 
-)
+    )
 
-select *
-from joined
+    select *
+    from joined
 
 {% else %}
 
