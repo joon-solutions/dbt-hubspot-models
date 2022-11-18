b7e4209ef1fde654d1f7025b54703c8457a34a12
My <my.hh@joonsolutions.com>
2022-11-18 13:58:33 +0700

    CA-53: add fct__hubspot__deal_stage model

---
+++ models/mart/hubspot/Sales/fct__hubspot__deal_stage.sql
@@ -0,0 +1,67 @@
+{{ config(enabled = var('hubspot__deal_stage') ) }}
+
+with deals as (
+
+    select *
+    from {{ ref('base__hubspot__deal') }}
+
+),
+
+deal_stage as (
+
+    select *
+    from {{ ref('base__hubspot__deal_stage') }}
+
+),
+
+pipeline_stage as (
+
+    select *
+    from {{ ref('base__hubspot__deal_pipeline_stage') }}
+
+),
+
+pipeline as (
+
+    select *
+    from {{ ref('base__hubspot__deal_pipeline') }}
+
+),
+
+final as (
+
+    select
+        deal_stage.deal_id || '-' || row_number() over(partition by deal_stage.deal_id order by deal_stage.date_entered asc) as deal_stage_id,
+        deals.deal_id,
+        deals.deal_name,
+        deal_stage._fivetran_start,
+        deal_stage._fivetran_end,
+        deal_stage._fivetran_active,
+
+        deal_stage.deal_stage_name,
+        pipeline_stage.pipeline_stage_label,
+        pipeline_stage.deal_pipeline_id,
+        pipeline.pipeline_label,
+        deal_stage.source,
+        deal_stage.source_id,
+        pipeline_stage.is_pipeline_stage_active,
+        pipeline.is_pipeline_active,
+        pipeline_stage.is_pipeline_stage_closed_won,
+        pipeline_stage.pipeline_stage_display_order,
+        pipeline.pipeline_display_order,
+        pipeline_stage.pipeline_stage_probability
+
+    from deal_stage
+
+    left join deals
+        on deal_stage.deal_id = deals.deal_id
+
+    left join pipeline_stage
+        on deals.deal_pipeline_stage_id = pipeline_stage.deal_pipeline_stage_id
+
+    left join pipeline
+        on pipeline_stage.deal_pipeline_id = pipeline.deal_pipeline_id
+)
+
+select *
+from final
---
+++ models/staging/hubspot/base/base__hubspot__deal_stage.sql
@@ -0,0 +1,23 @@
+{{ config(enabled = var('hubspot__deal_stage') ) }}
+
+with source as (
+
+    select * from {{ source('hubspot', 'deal_stage') }}
+
+),
+
+renamed as (
+
+    select
+        {{
+            fivetran_utils.fill_staging_columns(
+                source_columns=adapter.get_columns_in_relation(source('hubspot', 'deal_stage')),
+                staging_columns=get_hubspot_deal_stage_columns()
+            )
+        }}
+    from source
+
+)
+
+select *
+from renamed
