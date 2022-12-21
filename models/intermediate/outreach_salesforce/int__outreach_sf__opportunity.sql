5339db2c3a2e6866cd865ff69cadc20c6bfa2719
Na <nanguyenbg@gmail.com>
2022-12-21 18:34:11 +0700

    sqlfluff fix

--- models/intermediate/outreach_salesforce/int__outreach_sf__opportunity.sql
+++ models/intermediate/outreach_salesforce/int__outreach_sf__opportunity.sql
@@ -1,47 +1,49 @@
 with outreach as (
 
-    select  opportunity_id as outreach_opportunity_id,
-            opportunity_name,
-            account_id as outreach_account_id,
-            amount,
-            close_date,
-            opportunity_status,
-            opportunity_probability,
-            is_closed,
-            created_at,
-            'outreach' as source
+    select
+        opportunity_id as outreach_opportunity_id,
+        opportunity_name,
+        account_id as outreach_account_id,
+        amount,
+        close_date,
+        opportunity_status,
+        opportunity_probability,
+        is_closed,
+        created_at,
+        'outreach' as source
     from {{ ref('stg__outreach__opportunity') }}
 ),
 
 sf as (
-    select  opportunity_id as sf_opportunity_id,
-            opportunity_name,
-            account_id as sf_account_id,
-            amount,
-            close_date,
-            opportunity_status,
-            opportunity_probability,
-            is_closed,
-            created_date,
-            'sf' as source
-    from {{ ref('stg__salesforce__opportunity') }}    
+    select
+        opportunity_id as sf_opportunity_id,
+        opportunity_name,
+        account_id as sf_account_id,
+        amount,
+        close_date,
+        opportunity_status,
+        opportunity_probability,
+        is_closed,
+        created_date,
+        'sf' as source
+    from {{ ref('stg__salesforce__opportunity') }}
 ),
 
 final as (
-    select 
-            outreach.outreach_opportunity_id,
-            outreach.outreach_account_id,
-            sf.sf_opportunity_id,
-            sf.sf_account_id,
-            nvl(outreach.opportunity_name, sf.opportunity_name) as opportunity_name,
-            nvl(outreach.amount, sf.amount) as amount,
-            nvl(outreach.close_date, sf.close_date) as close_date,
-            nvl(outreach.opportunity_status, sf.opportunity_status) as opportunity_status,
-            nvl(outreach.opportunity_probability, sf.opportunity_probability) as opportunity_probability,
-            nvl(outreach.is_closed, sf.is_closed) as is_closed,
-            nvl(outreach.created_at, sf.created_date) as created_at,
-            nvl(outreach.source, sf.source) as source,
-            {{ dbt_utils.surrogate_key(['outreach.outreach_opportunity_id', 'sf.sf_opportunity_id']) }} as opportunity_id
+    select
+        outreach.outreach_opportunity_id,
+        outreach.outreach_account_id,
+        sf.sf_opportunity_id,
+        sf.sf_account_id,
+        COALESCE(outreach.opportunity_name, sf.opportunity_name) as opportunity_name,
+        COALESCE(outreach.amount, sf.amount) as amount,
+        COALESCE(outreach.close_date, sf.close_date) as close_date,
+        COALESCE(outreach.opportunity_status, sf.opportunity_status) as opportunity_status,
+        COALESCE(outreach.opportunity_probability, sf.opportunity_probability) as opportunity_probability,
+        COALESCE(outreach.is_closed, sf.is_closed) as is_closed,
+        COALESCE(outreach.created_at, sf.created_date) as created_at,
+        COALESCE(outreach.source, sf.source) as source,
+        {{ dbt_utils.surrogate_key(['outreach.outreach_opportunity_id', 'sf.sf_opportunity_id']) }} as opportunity_id
 
     from outreach
     full outer join sf on outreach.opportunity_name = sf.opportunity_name
