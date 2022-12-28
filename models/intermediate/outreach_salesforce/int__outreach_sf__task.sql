7c3cd8212375964c1dec049be28ad7c85d86fb05
Na <nanguyenbg@gmail.com>
2022-12-28 16:26:46 +0700

    sqlfluff fix

--- models/intermediate/outreach_salesforce/int__outreach_sf__task.sql
+++ models/intermediate/outreach_salesforce/int__outreach_sf__task.sql
@@ -1,26 +1,26 @@
 with sf as (
-    select  
-            task_id as sf_task_id,
-            account_id as sf_account_id,
-            null as outreach_task_id,
-            null as outreach_account_id,
-            task_action_type,
-            completed_at,
-            is_completed,
-            'salesforce' as source
+    select
+        task_id as sf_task_id,
+        account_id as sf_account_id,
+        null as outreach_task_id,
+        null as outreach_account_id,
+        task_action_type,
+        completed_at,
+        is_completed,
+        'salesforce' as source
     from {{ ref('base__salesforce__task') }}
-), 
+),
 
 outreach as (
-    select  
-            null as sf_task_id,
-            null as sf_account_id,
-            task_id as outreach_task_id,
-            account_id as outreach_account_id,
-            task_action_type,
-            completed_at,
-            is_completed,
-            'outreach' as source
+    select
+        null as sf_task_id,
+        null as sf_account_id,
+        task_id as outreach_task_id,
+        account_id as outreach_account_id,
+        task_action_type,
+        completed_at,
+        is_completed,
+        'outreach' as source
     from {{ ref('base__outreach__task') }}
 ),
 
@@ -30,7 +30,7 @@ final as (
     select * from outreach
 )
 
-select 
-        *,
-        {{ dbt_utils.surrogate_key(['sf_task_id','outreach_task_id','source']) }} as task_id
+select
+    *,
+    {{ dbt_utils.surrogate_key(['sf_task_id','outreach_task_id','source']) }} as task_id
 from final
