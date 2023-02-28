283a2065cb9e6bab9ad6dc9c74f41b5e9c1b6b85
My <my.hh@joonsolutions.com>
2023-02-28 09:57:00 +0700

    update int model

--- models/intermediate/salesforce/int__salesforce__agg_by_owner.sql
+++ models/intermediate/salesforce/int__salesforce__agg_by_owner.sql
@@ -4,22 +4,14 @@ with opportunity as (
     from {{ ref('stg__salesforce__opportunity') }}
 ),
 
-users as (
-    select
-        opportunity_owner_id,
-        opportunity_owner_name,
-        opportunity_owner_city,
-        opportunity_owner_state
-
-    from opportunity
-    group by 1, 2, 3, 4
-),
-
 booking_by_owner as (
 
     select
         opportunity_manager_id as b_manager_id,
         opportunity_owner_id as b_owner_id,
+        opportunity_owner_name as b_owner_name,
+        opportunity_owner_city as b_owner_city,
+        opportunity_owner_state as b_owner_state,
         round(sum(closed_amount_this_month)) as bookings_amount_closed_this_month,
         round(sum(closed_amount_this_quarter)) as bookings_amount_closed_this_quarter,
         count(*) as total_number_bookings,
@@ -31,7 +23,7 @@ booking_by_owner as (
         avg(days_to_close) as avg_days_to_close
     from opportunity
     where opportunity_status = 'Won'
-    group by 1, 2
+    group by 1, 2, 3, 4, 5
 ),
 
 lost_by_owner as (
@@ -39,6 +31,9 @@ lost_by_owner as (
     select
         opportunity_manager_id as l_manager_id,
         opportunity_owner_id as l_owner_id,
+        opportunity_owner_name as l_owner_name,
+        opportunity_owner_city as l_owner_city,
+        opportunity_owner_state as l_owner_state,
         round(sum(closed_amount_this_month)) as lost_amount_this_month,
         round(sum(closed_amount_this_quarter)) as lost_amount_this_quarter,
         count(*) as total_number_lost,
@@ -47,7 +42,7 @@ lost_by_owner as (
         sum(closed_count_this_quarter) as lost_count_this_quarter
     from opportunity
     where opportunity_status = 'Lost'
-    group by 1, 2
+    group by 1, 2, 3, 4, 5
 ),
 
 pipeline_by_owner as (
@@ -55,6 +50,9 @@ pipeline_by_owner as (
     select
         opportunity_manager_id as p_manager_id,
         opportunity_owner_id as p_owner_id,
+        opportunity_owner_name as p_owner_name,
+        opportunity_owner_city as p_owner_city,
+        opportunity_owner_state as p_owner_state,
         round(sum(created_amount_this_month)) as pipeline_created_amount_this_month,
         round(sum(created_amount_this_quarter)) as pipeline_created_amount_this_quarter,
         round(sum(created_amount_this_month * opportunity_probability)) as pipeline_created_forecast_amount_this_month,
@@ -69,19 +67,20 @@ pipeline_by_owner as (
         avg(days_since_created) as avg_days_open
     from opportunity
     where opportunity_status = 'Pipeline'
-    group by 1, 2
+    group by 1, 2, 3, 4, 5
 )
 
 select
-    users.*,
     coalesce(pipeline_by_owner.p_manager_id, booking_by_owner.b_manager_id, lost_by_owner.l_manager_id) as manager_id,
+    coalesce(pipeline_by_owner.p_owner_id, booking_by_owner.b_owner_id, lost_by_owner.l_owner_id) as owner_id,
+    coalesce(pipeline_by_owner.p_owner_name, booking_by_owner.b_owner_name, lost_by_owner.l_owner_name) as owner_name,
+    coalesce(pipeline_by_owner.p_owner_city, booking_by_owner.b_owner_city, lost_by_owner.l_owner_city) as owner_city,
+    coalesce(pipeline_by_owner.p_owner_state, booking_by_owner.b_owner_state, lost_by_owner.l_owner_state) as owner_state,
     booking_by_owner.*,
     lost_by_owner.*,
     pipeline_by_owner.*
-from users
-left join booking_by_owner
-    on booking_by_owner.b_owner_id = users.opportunity_owner_id
-left join lost_by_owner
-    on lost_by_owner.l_owner_id = users.opportunity_owner_id
-left join pipeline_by_owner
-    on pipeline_by_owner.p_owner_id = users.opportunity_owner_id
+from booking_by_owner
+full outer join lost_by_owner
+    on lost_by_owner.l_owner_id = booking_by_owner.b_owner_id
+full outer join pipeline_by_owner
+    on pipeline_by_owner.p_owner_id = booking_by_owner.b_owner_id
