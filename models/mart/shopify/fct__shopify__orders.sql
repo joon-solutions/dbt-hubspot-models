64fd3c9156d49930e7fac45d18d7c9dafd57ef25
My <my.hh@joonsolutions.com>
2023-04-13 16:14:57 +0700

    update based on feedback

--- models/mart/shopify/fct__shopify__orders.sql
+++ models/mart/shopify/fct__shopify__orders.sql
@@ -46,8 +46,8 @@ fulfillments as (
     select
         order_id,
         source_relation,
-        count(fulfillment_id) as count_fulfillments,
-        count(fulfillment_event_id) as count_fulfillment_events,
+        count(distinct fulfillment_id) as count_fulfillments,
+        count(distinct fulfillment_event_id) as count_fulfillment_events,
         {{ fivetran_utils.string_agg("distinct cast(service as " ~ dbt_utils.type_string() ~ ")", "', '") }} as fulfillment_services,
         {{ fivetran_utils.string_agg("distinct cast(tracking_company as " ~ dbt_utils.type_string() ~ ")", "', '") }} as tracking_companies,
         {{ fivetran_utils.string_agg("distinct cast(tracking_number as " ~ dbt_utils.type_string() ~ ")", "', '") }} as tracking_numbers
