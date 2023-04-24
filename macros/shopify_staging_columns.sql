5c97b614d0e4e0477700d5d88ebaa90dc9904541
My <my.hh@joonsolutions.com>
2023-04-24 14:22:08 +0700

    update fulfillment model

--- macros/shopify_staging_columns.sql
+++ macros/shopify_staging_columns.sql
@@ -552,4 +552,27 @@
 
 {% endmacro %}
 
+{% macro get_shopify_order_shipping_tax_line_columns() %}
+
+{% set columns = [
+    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
+    {"name": "carrier_identifier", "datatype": dbt_utils.type_string()},
+    {"name": "code", "datatype": dbt_utils.type_string()},
+    {"name": "delivery_category", "datatype": dbt_utils.type_string()},
+    {"name": "discounted_price", "datatype": dbt_utils.type_float()},
+    {"name": "discounted_price_set", "datatype": dbt_utils.type_string()},
+    {"name": "id", "datatype": dbt_utils.type_int(), "alias": "order_shipping_line_id"},
+    {"name": "order_id", "datatype": dbt_utils.type_int()},
+    {"name": "phone", "datatype": dbt_utils.type_string()},
+    {"name": "price", "datatype": dbt_utils.type_float()},
+    {"name": "price_set", "datatype": dbt_utils.type_string()},
+    {"name": "requested_fulfillment_service_id", "datatype": dbt_utils.type_string()},
+    {"name": "source", "datatype": dbt_utils.type_string()},
+    {"name": "title", "datatype": dbt_utils.type_string()}
+] %}
+
+{{ return(columns) }}
+
+{% endmacro %}
+
 
