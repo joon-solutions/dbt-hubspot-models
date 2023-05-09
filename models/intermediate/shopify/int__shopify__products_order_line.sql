87afb99aa031fce7395844873f421f59167a5a4e
Na <nanguyenbg@gmail.com>
2023-05-09 18:37:30 +0700

    CA-122 Product Basket Analysis

---
+++ models/intermediate/shopify/int__shopify__products_order_line.sql
@@ -0,0 +1,30 @@
+with order_lines as (
+
+    select *
+    from {{ ref('stg__shopify__order_lines') }}
+
+),
+
+products as (
+    select *
+    from {{ ref('stg__shopify__products') }}
+),
+
+final as (
+    select
+        products.*,
+        order_lines.order_line_globalid,
+        order_lines.order_globalid,
+        order_lines.order_route,
+        order_lines.total_discount as order_line_discount,
+        order_lines.quantity as order_line_quantity,
+        order_lines.pre_tax_price as order_line_pre_tax_price,
+        order_lines.order_line_tax
+    from products
+    left join order_lines on products.product_globalid = order_lines.product_globalid --one-to-many
+)
+
+select
+    *,
+    {{ dbt_utils.surrogate_key(['order_line_globalid','product_globalid']) }} as id
+from final
--- models/mart/shopify/fct__shopify__product_basket.sql
+++ models/mart/shopify/fct__shopify__product_basket.sql
@@ -1,40 +1,35 @@
 with product as (
-    select * from {{ ref('base__shopify__product') }}
+    select * from {{ ref('int__shopify__products_order_line') }}
 ),
 
+--to get orders attributes
 orders as (
-    select * from {{ ref('stg__shopify__product_order_aggregates') }}
-),
-
-joined as (
-    select
-        orders.order_globalid,
-        orders.customer_globalid,
-        orders.closed_timestamp,
-        product.product_globalid,
-        product.handle,
-        product.product_type,
-        product.title,
-        product.vendor,
-        product.status
-    from orders
-    inner join product on product.product_globalid = orders.product_globalid
+    select * from {{ ref('int__shopify__orders') }}
 ),
 
 basket as (
     select
-        concat(product_globalid, ',') as products_list,
-        concat(product_type) as products_type_list,
-        concat(handle) as products_handle_list,
-        concat(title) as products_title_list,
-        count(*) as frequency
+        order_globalid,
+        listagg(distinct product_globalid, ',') as products_list,
+        listagg(distinct tags, ',') as product_tag_list,
+        listagg(distinct product_type, ',') as products_type_list,
+        listagg(distinct handle, ',') as products_handle_list,
+        listagg(distinct title, ',') as products_title_list
+    from product
+    group by 1
+),
 
-    from joined
-    group by product_globalid, product_type, handle, title
-    order by frequency desc
+final as (
+    select
+        orders.*, ---in BI layer we can pivot by order times/ order amounts/ or other order attributes to dig further into each product combo performance
+        basket.products_list,
+        basket.product_tag_list,
+        basket.products_type_list,
+        basket.products_handle_list,
+        basket.products_title_list
+    from basket
+    left join orders on basket.order_globalid = orders.order_globalid --one-to-one
 )
 
-select
-    *,
-    {{ dbt_utils.surrogate_key(['products_list','frequency']) }} as basket_id
-from basket
+select *
+from final
