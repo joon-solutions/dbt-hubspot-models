e58c94dddc061b4565eaae3c1d3283cae860f805
Will Pride <wpride@canvasapp.com>
2023-05-19 10:11:06 -0700

    Remove default database

--- models/tmp/shopify/base__shopify__refund_tmp.sql
+++ models/tmp/shopify/base__shopify__refund_tmp.sql
@@ -3,8 +3,7 @@
 {{
 fivetran_utils.union_data(
     table_identifier='refund', 
-    default_database='joon_solutions',
-    default_schema='shopify',
+        default_schema='shopify',
     union_schema_variable='shopify_union_schemas',
     union_database_variable='shopify_union_databases'
 )
