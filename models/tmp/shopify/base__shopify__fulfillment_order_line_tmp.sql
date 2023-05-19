038f5eb113d5e842aad2f5418efcc8378baefc91
Will Pride <wpride@canvasapp.com>
2023-05-19 10:13:56 -0700

    target.database

--- models/tmp/shopify/base__shopify__fulfillment_order_line_tmp.sql
+++ models/tmp/shopify/base__shopify__fulfillment_order_line_tmp.sql
@@ -3,7 +3,7 @@
 {{
 fivetran_utils.union_data(
     table_identifier='fulfillment_order_line', 
-    default_database='joon_solutions',
+    default_database=target.database,
     default_schema='shopify',
     union_schema_variable='shopify_union_schemas',
     union_database_variable='shopify_union_databases'
--- models/tmp/shopify/base__shopify__transaction_tmp.sql
+++ models/tmp/shopify/base__shopify__transaction_tmp.sql
@@ -2,7 +2,7 @@
 {{
 fivetran_utils.union_data(
     table_identifier='transaction', 
-    default_database='joon_solutions',
+    default_database=target.database,
     default_schema='shopify',
     union_schema_variable='shopify_union_schemas',
     union_database_variable='shopify_union_databases'
