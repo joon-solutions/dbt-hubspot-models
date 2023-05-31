9089e3cff4350c677f1d1f2daadd28584f4657f1
Uyen Bui <uyen.bt@joonsolutions.com>
2023-05-31 14:30:30 +0700

    adjust fraud logic, base__location

--- models/mart/shopify/fct__shopify__transactions.sql
+++ models/mart/shopify/fct__shopify__transactions.sql
@@ -5,22 +5,33 @@ with transactions as (
 
     select
         *,
+        case
+            when extract(minute from created_timestamp) <= 10 then 10
+            when extract(minute from created_timestamp) <= 20 then 20
+            when extract(minute from created_timestamp) <= 30 then 30
+            when extract(minute from created_timestamp) <= 40 then 40
+            when extract(minute from created_timestamp) <= 50 then 50
+            else 60 end as created_minute,
         extract(hour from created_timestamp) as created_hour,
-        date(created_timestamp) as created_date
+        date(created_timestamp) as created_date,
+        date_trunc(week, created_timestamp) as created_week
     from {{ ref('stg__shopify__transactions') }}
 
 ),
 
-fraud_detection as (
+fraud_count as (
 
     select
         *,
-        --how many different credit cards a certain user id has attempted within an hour
+        --how many times a credit card has been used within 10 mins
         count(distinct order_globalid) over (
-            partition by payment_credit_card_number, created_hour, created_date) as fraud_credit_card_count,
-        --how many times a credit card has been used within a certain time period
+            partition by payment_credit_card_number, created_minute, created_hour, created_date) as order_by_credit_card,
+        --how many different credit cards a certain user id has attempted within 10 mins
         count(distinct payment_credit_card_number) over (
-            partition by payment_credit_card_number, created_hour, created_date) as fraud_user_count
+            partition by user_id, created_minute, created_hour, created_date) as credit_card_by_user,
+        --how many void transactions a certain user id has within 7 days
+        count(distinct case when kind = 'void' then transaction_globalid end) over (
+            partition by user_id, created_week) as void_transaction_by_user
     from transactions
 
 ),
@@ -29,12 +40,27 @@ final as (
 
     select
         *,
+        --fraud score
         case
-            when fraud_credit_card_count > 2 then 'Fraud credit card'
-            when fraud_credit_card_count > 2 then 'Fraud user'
-            else 'No fraud' end as fraud_detection
+            when order_by_credit_card >= 5 then 1
+            else 0 end as fraud_credit_card_score,
+        case
+            when credit_card_by_user >= 2 then 1
+            else 0 end as fraud_user_score,
+        case
+            when void_transaction_by_user >= 5 then 1
+            else 0 end as fraud_void_transation_score,
+        case
+            when order_by_credit_card >= 5 then 1
+            else 0 end +
+        case
+            when credit_card_by_user >= 2 then 1
+            else 0 end +
+        case
+            when void_transaction_by_user >= 5 then 1
+            else 0 end as fraud_score
 
-    from fraud_detection
+    from fraud_count
 
 )
 
