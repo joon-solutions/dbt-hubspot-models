bf32dd27f6da3116b1367b817cb7af94cf130ec1
Uyen Bui <uyen.bt@joonsolutions.com>
2023-05-31 15:51:26 +0700

    add sliding window frame

--- models/mart/shopify/fct__shopify__transactions.sql
+++ models/mart/shopify/fct__shopify__transactions.sql
@@ -5,16 +5,9 @@ with transactions as (
 
     select
         *,
-        case
-            when extract(minute from created_timestamp) <= 10 then 10
-            when extract(minute from created_timestamp) <= 20 then 20
-            when extract(minute from created_timestamp) <= 30 then 30
-            when extract(minute from created_timestamp) <= 40 then 40
-            when extract(minute from created_timestamp) <= 50 then 50
-            else 60 end as created_minute,
-        extract(hour from created_timestamp) as created_hour,
-        date(created_timestamp) as created_date,
-        date_trunc(week, created_timestamp) as created_week
+        time_slice(created_timestamp, 1, 'minute') as created_minute,
+        -- extract(hour from created_timestamp) as created_hour,
+        date(created_timestamp) as created_date
     from {{ ref('stg__shopify__transactions') }}
 
 ),
@@ -24,14 +17,14 @@ fraud_count as (
     select
         *,
         --how many times a credit card has been used within 10 mins
-        count(distinct order_globalid) over (
-            partition by payment_credit_card_number, created_minute, created_hour, created_date) as order_by_credit_card,
+        count(transaction_globalid) over (
+            partition by payment_credit_card_number order by created_minute rows between 10 preceding and current row) as order_by_credit_card,
         --how many different credit cards a certain user id has attempted within 10 mins
-        count(distinct payment_credit_card_number) over (
-            partition by user_id, created_minute, created_hour, created_date) as credit_card_by_user,
+        count(payment_credit_card_number) over (
+            partition by user_id order by created_minute rows between 10 preceding and current row) as credit_card_by_user,
         --how many void transactions a certain user id has within 7 days
-        count(distinct case when kind = 'void' then transaction_globalid end) over (
-            partition by user_id, created_week) as void_transaction_by_user
+        count(case when kind = 'void' then transaction_globalid end) over (
+            partition by user_id order by created_date rows between 7 preceding and current row) as void_transaction_by_user
     from transactions
 
 ),
