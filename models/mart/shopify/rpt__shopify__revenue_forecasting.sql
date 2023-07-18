da75f8ad331f93f162c9f9add65f1878b1bfeb33
nguyendnb <nguyen.dnb@joonsolutions.com>
2023-07-18 17:16:36 +0700

    make fix

--- models/mart/shopify/rpt__shopify__revenue_forecasting.sql
+++ models/mart/shopify/rpt__shopify__revenue_forecasting.sql
@@ -17,13 +17,13 @@ recent_orders as (
     select
         orders_joined.sku_globalid,
         orders_joined.sku,
-        coalesce(sum(orders_joined.order_items_count),0) as order_items_count,
-        coalesce(sum(orders_joined.price_sum),0) as price_sum,
-        coalesce(div0(sum(orders_joined.price_sum),sum(orders_joined.order_items_count)),0) as avg_price_per_item
+        coalesce(sum(orders_joined.order_items_count), 0) as order_items_count,
+        coalesce(sum(orders_joined.price_sum), 0) as price_sum,
+        coalesce(div0(sum(orders_joined.price_sum), sum(orders_joined.order_items_count)), 0) as avg_price_per_item
     from orders_joined
     where {{ dbt_utils.date_trunc('month','orders_joined.order_date') }} = {{ dbt_utils.date_trunc('month','orders_joined.first_order_date') }} -- mapping with first_order_date for better visualization
     --{{ dbt_utils.date_trunc('month','current_date') }}
-    group by 1,2
+    group by 1, 2
 
 ),
 
@@ -38,7 +38,7 @@ final as (
         recent_orders.avg_price_per_item,
         demand_forecasting.forecasted_quantity * recent_orders.avg_price_per_item as forecasted_revenue
     from demand_forecasting
-    left join recent_orders 
+    left join recent_orders
         on demand_forecasting.sku_globalid = recent_orders.sku_globalid
 
 )
