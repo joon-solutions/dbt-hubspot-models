59118055a4cc43f63c8df1156461b9dec41b7284
Will Pride <wpride@canvasapp.com>
2023-08-10 15:55:59 -0700

    Use dbt dateadd

--- models/staging/shopify/base/base__shopify__calendar_forecasting.sql
+++ models/staging/shopify/base/base__shopify__calendar_forecasting.sql
@@ -1,6 +1,6 @@
 {{ dbt_utils.date_spine(
     datepart="day",
     start_date=var('shopify_base_dates'),
-    end_date="dateadd(year, 1, date_trunc(month, current_date()))"
+    end_date="dbt.dateadd(year, 1, date_trunc(month, current_date()))"
    )
 }}
